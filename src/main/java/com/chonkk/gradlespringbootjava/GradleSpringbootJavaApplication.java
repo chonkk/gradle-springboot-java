45c6d2721668fd7d69b8b2854d69552fbe5e876b
천광규 <chonkk@castis.com>
2021-06-28 13:21:59 +0900

    Initial commit

---
+++ src/main/java/com/chonkk/gradlespringbootjava/GradleSpringbootJavaApplication.java
@@ -0,0 +1,13 @@
+package com.chonkk.gradlespringbootjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleSpringbootJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleSpringbootJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chonkk/gradlespringbootjava/GradleSpringbootJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chonkk.gradlespringbootjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleSpringbootJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
